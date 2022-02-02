f6fffe580b5f3c2398dd6f4354d86cc252bb646d
Tung Le <tunglexuan2002@gmail.com>
2022-02-02 18:58:38 +0700

    post method

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine 
 app.engine('hbs', engine({
@@ -23,7 +28,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+    console.log(req.query.q)
     res.render('news')
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body)
+    res.send('')
 })
 
 app.listen(port, () => {
