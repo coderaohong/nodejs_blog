08c463b925e8a133a09fdc1cff53031bb2d48847
Tung Le <tunglexuan2002@gmail.com>
2022-02-03 22:31:47 +0700

    refacter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-const { engine } = require('express-handlebars');
-const path = require('path');
+const { engine } = require('express-handlebars')
+const path = require('path')
 const app = express()
 const port = 3000
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -23,23 +24,8 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources\\views'))
 console.log(__dirname)
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/news', (req, res) => {
-    console.log(req.query.q)
-    res.render('news')
-})
-
-app.get('/search', (req, res) => {
-    res.render('search')
-})
-
-app.post('/search', (req, res) => {
-    console.log(req.body)
-    res.send('')
-})
+// Route init
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+
+function route(app) {
+
+    app.use('/news', newsRouter)
+
+    app.use('/', siteRouter)
+    // app.get('/news', (req, res) => {
+    //     console.log(req.query.q)
+    //     res.render('news')
+    // })
+}
+
+module.exports = route
