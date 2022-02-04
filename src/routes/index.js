149aaaf0c12cfe7e5dc0b894cda4817620a597fe
Tung Le <tunglexuan2002@gmail.com>
2022-02-04 19:07:14 +0700

    install prettier, lint-staged, husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
-const newsRouter = require('./news')
-const siteRouter = require('./site')
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 
 function route(app) {
+    app.use('/news', newsRouter);
 
-    app.use('/news', newsRouter)
-
-    app.use('/', siteRouter)
+    app.use('/', siteRouter);
     // app.get('/news', (req, res) => {
     //     console.log(req.query.q)
     //     res.render('news')
     // })
 }
 
-module.exports = route
+module.exports = route;
